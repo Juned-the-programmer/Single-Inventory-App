3f0f8777654e010d2fe6bd001e637eca703f31df
Juned-the-programmer <davdajuned7698@gmail.com>
2021-09-05 23:34:40 +0530

    Final Push Estimate

--- dashboard/views.py
+++ dashboard/views.py
@@ -1826,130 +1826,172 @@ def customer_payment_list(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        customer_payment = customerpay_estimate.objects.all()
-    else:
-        customer_payment = customerpay_gst.objects.all()
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                customer_payment = customerpay_estimate.objects.all()
+            else:
+                customer_payment = customerpay_gst.objects.all()
 
-    context = {
-        'customer_payment':customer_payment
-    }
-    return render(request, 'dashboard/customerpaymentlist.html',context)
+            context = {
+                'customer_payment':customer_payment
+            }
+            return render(request, 'dashboard/customerpaymentlist.html',context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def supplier_payment_list(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        supplier_payment = supplierpay_estimate.objects.all()
-    else:
-        supplier_payment = supplierpay_gst.objects.all()
-    
-    context = {
-        'supplier_payment':supplier_payment
-    }
-    return render(request,"dashboard/supplierpaymentlist.html",context)
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                supplier_payment = supplierpay_estimate.objects.all()
+            else:
+                supplier_payment = supplierpay_gst.objects.all()
+            
+            context = {
+                'supplier_payment':supplier_payment
+            }
+            return render(request,"dashboard/supplierpaymentlist.html",context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def customer_Credit(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        customer_credit_data = customeraccount_estimate.objects.all()
-    else:
-        customer_credit_data = customeraccount_gst.objects.all()
-    
-    context = {
-        'customer_credit_data':customer_credit_data
-    }
-    return render(request, 'dashboard/customercredit.html',context)
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                customer_credit_data = customeraccount_estimate.objects.all()
+            else:
+                customer_credit_data = customeraccount_gst.objects.all()
+            
+            context = {
+                'customer_credit_data':customer_credit_data
+            }
+            return render(request, 'dashboard/customercredit.html',context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def supplier_credit(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        supplier_credit_list = supplieraccount_estimate.objects.all()
-    else:
-        supplier_credit_list = supplieraccount_gst.objects.all()
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                supplier_credit_list = supplieraccount_estimate.objects.all()
+            else:
+                supplier_credit_list = supplieraccount_gst.objects.all()
 
-    context = {
-        'supplier_credit_list':supplier_credit_list
-    }
-    return render(request,'dashboard/suppliercredit.html',context)
+            context = {
+                'supplier_credit_list':supplier_credit_list
+            }
+            return render(request,'dashboard/suppliercredit.html',context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def totalincome(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        daily_income_data = dailyincome_estimate.objects.all()
-        customer_income_data = customerpay_estimate.objects.all()
-    else:
-        daily_income_data = dailyincome_gst.objects.all()
-        customer_income_data = customerpay_gst.objects.all()
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                daily_income_data = dailyincome_estimate.objects.all()
+                customer_income_data = customerpay_estimate.objects.all()
+            else:
+                daily_income_data = dailyincome_gst.objects.all()
+                customer_income_data = customerpay_gst.objects.all()
 
-    context = {
-        'daily_income_data':daily_income_data,
-        'customer_income_data':customer_income_data
-    }
-    return render(request,'dashboard/totalincome.html',context)
+            context = {
+                'daily_income_data':daily_income_data,
+                'customer_income_data':customer_income_data
+            }
+            return render(request,'dashboard/totalincome.html',context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def totalexpense(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        daily_expense_data = dailyexpense_estimate.objects.all()
-        supplier_expense_data = supplierpay_estimate.objects.all()
-    else:
-        daily_expense_data = dailyexpense_gst.objects.all()
-        supplier_expense_data = supplierpay_gst.objects.all()
-    
-    context = {
-        'daily_expense_data':daily_expense_data,
-        'supplier_expense_data':supplier_expense_data
-    }
-    return render(request,'dashboard/totalexpense.html',context)
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                daily_expense_data = dailyexpense_estimate.objects.all()
+                supplier_expense_data = supplierpay_estimate.objects.all()
+            else:
+                daily_expense_data = dailyexpense_gst.objects.all()
+                supplier_expense_data = supplierpay_gst.objects.all()
+            
+            context = {
+                'daily_expense_data':daily_expense_data,
+                'supplier_expense_data':supplier_expense_data
+            }
+            return render(request,'dashboard/totalexpense.html',context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def salereport(request):
     Estimate_group = Group.objects.get(name='Estimate')
     GST_group = Group.objects.get(name='GST')
     user = User.objects.get(username=request.user.username)
-    if Estimate_group in user.groups.all():
-        if request.method == 'POST':
-            if 'Estimate' in request.POST:
-                searchsale = Estimate_sales.objects.filter(date__gte = request.POST['fromdate'] , date__lte = request.POST['todate'])
+    try:
+        if user.is_authenticated:
+            if Estimate_group in user.groups.all():
+                if request.method == 'POST':
+                    if 'Estimate' in request.POST:
+                        searchsale = Estimate_sales.objects.filter(date__gte = request.POST['fromdate'] , date__lte = request.POST['todate'])
 
-                context = {
-                    'searchsale' : searchsale
-                }
-                return render(request,'dashboard/salereport.html',context)
+                        context = {
+                            'searchsale' : searchsale
+                        }
+                        return render(request,'dashboard/salereport.html',context)
 
-    if GST_group in user.groups.all():
-        if request.method == 'POST':
-            if 'GST' in request.POST:
-                searchsale = gstsale.objects.filter(date__gte = request.POST['fromdate'] , date__lte = request.POST['todate'])
+            if GST_group in user.groups.all():
+                if request.method == 'POST':
+                    if 'GST' in request.POST:
+                        searchsale = gstsale.objects.filter(date__gte = request.POST['fromdate'] , date__lte = request.POST['todate'])
 
-                context = {
-                    'searchsale' : searchsale
-                }
-                return render(request,'dashboard/salereport.html',context)
+                        context = {
+                            'searchsale' : searchsale
+                        }
+                        return render(request,'dashboard/salereport.html',context)
 
-    total_estimate_sale = Estimate_sales.objects.all()
-    total_gst_sale = gstsale.objects.all()
-    
-    context = {
-        'total_estimate_sale' : total_estimate_sale,
-        'total_gst_sale' : total_gst_sale,
-    }
-    return render(request,'dashboard/salereport.html',context)
+            total_estimate_sale = Estimate_sales.objects.all()
+            total_gst_sale = gstsale.objects.all()
+            
+            context = {
+                'total_estimate_sale' : total_estimate_sale,
+                'total_gst_sale' : total_gst_sale,
+            }
+            return render(request,'dashboard/salereport.html',context)
+        else:
+            return redirect('login')
+    except:
+        return redirect('error404')
 
 @login_required(login_url='login')
 def getsupplier(request):
